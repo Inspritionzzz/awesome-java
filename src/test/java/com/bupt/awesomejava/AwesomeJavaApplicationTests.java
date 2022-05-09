3dd7c0ba6696f55cc0b1e457b002114e1f96108f
jason <591831416@qq.com>
2022-05-09 09:42:19 +0800

    ''

---
+++ src/test/java/com/bupt/awesomejava/AwesomeJavaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bupt.awesomejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("just a test");
+	}
+
+}

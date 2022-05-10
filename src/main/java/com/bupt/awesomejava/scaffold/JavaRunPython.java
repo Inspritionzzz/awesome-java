7ff5c9e8732fb1ba376ff8253cb8b976d4ae9670
jason <591831416@qq.com>
2022-05-10 19:00:06 +0800

    ''

--- src/main/java/com/bupt/awesomejava/scaffold/JavaRunPython.java
+++ src/main/java/com/bupt/awesomejava/scaffold/JavaRunPython.java
@@ -13,21 +13,30 @@ public class JavaRunPython {
         f3();
     }
 
+    /**
+     * 使用jython直接执行python代码;
+     */
     public static void f1() {
         PythonInterpreter pythonInterpreter = new PythonInterpreter();
         pythonInterpreter.exec("str = 'just a test';");
         pythonInterpreter.exec("print str;");
     }
 
+    /**
+     * 使用jython直接执行python脚本;
+     */
     public static void f2() {
         PythonInterpreter pythonInterpreter = new PythonInterpreter();
         pythonInterpreter.execfile("C:\\JavaProject\\awesome-java\\src\\main\\java\\com\\bupt\\awesomejava\\scaffold\\python_script\\pydemo1.py");
     }
 
+    /**
+     * 调用python脚本;
+     */
     public static void f3() {
         Process proc;
         try {
-            // 大括号中，第一个双引号里是python解释器的路径，第二个双引号里是要执行的python文件的路径
+            // 大括号中,第一个双引号里是python解释器的路径,第二个双引号里是要执行的python文件的路径
             String[] a = new String[]{"C:\\Users\\zhangcy4\\AppData\\Local\\Programs\\Python\\Python38\\python.exe",
                     "C:\\JavaProject\\awesome-java\\src\\main\\java\\com\\bupt\\awesomejava\\scaffold\\python_script\\pydemo1.py"};
             proc = Runtime.getRuntime().exec(a);

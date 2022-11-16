3a59298f376484838d9e83afbe61085403f7c6f6
zhangcy <zhangchengyu@csc.com.cn>
2022-11-16 09:20:20 +0800

    'update-java-20221116'

---
+++ src/main/java/com/bupt/awesomejava/javacore/EffectiveCommonLib.java
@@ -0,0 +1,20 @@
+package com.bupt.awesomejava.javacore;
+
+/**
+ * @Description: 常用类库积累
+ * Created by jason on 2022/11/16 8:59
+ */
+
+public class EffectiveCommonLib {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 日期类库：
+     *  Date、LocalDate
+     */
+    void f1() {
+
+    }
+}
---
+++ src/main/java/com/bupt/awesomejava/javacore/EffectiveOOP.java
@@ -0,0 +1,33 @@
+package com.bupt.awesomejava.javacore;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * @Description: OOP
+ * Created by jason on 2022/11/16 8:46
+ */
+
+public class EffectiveOOP {
+    public static void main(String[] args) {
+        Date();
+    }
+
+    static void Date() {
+        System.out.println(new Date());
+        Date deadLine = new Date();
+        System.out.println(LocalDate.now());
+
+        LocalDate newYearEve = LocalDate.of(1999, 12, 31);
+        int year = newYearEve.getYear();
+        int month = newYearEve.getMonthValue();
+        int day = newYearEve.getDayOfMonth();
+        System.out.println(year + "-" + month + "-" + day);
+
+        LocalDate aThousandDaysLater = newYearEve.plusDays(1000);
+        year = aThousandDaysLater.getYear();
+        month = aThousandDaysLater.getMonthValue();
+        day = aThousandDaysLater.getDayOfMonth();
+        System.out.println(year + "-" + month + "-" + day);
+    }
+}

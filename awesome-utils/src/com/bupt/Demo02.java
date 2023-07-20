8f0be49f12db76b0d0bb0059889a2ca385cc4a19
jason <591831416@qq.com>
2023-07-20 08:29:06 +0800

    update-20230720

--- awesome-utils/src/com/bupt/Demo02.java
+++
@@ -1,7 +0,0 @@
-package com.bupt;
-
-public class Demo02 {
-    public static void main(String[] args) {
-        System.out.println("just a test");
-    }
-}
--- src/main/java/com/bupt/awesomejava/Scratch.java
+++ src/main/java/com/bupt/awesomejava/Scratch.java
@@ -1,4 +1,7 @@
 package com.bupt.awesomejava;
 
 public class Scratch {
+    public static void main(String[] args) {
+        System.out.println("just a test");
+    }
 }

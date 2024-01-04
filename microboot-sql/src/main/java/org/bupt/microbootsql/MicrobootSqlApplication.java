a09c0f3db45998006c459b631c4901d04700969b
jason <591831416@qq.com>
2024-01-04 17:03:01 +0800

    update-20240104

---
+++ microboot-sql/src/main/java/org/bupt/microbootsql/MicrobootSqlApplication.java
@@ -0,0 +1,13 @@
+package org.bupt.microbootsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobootSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrobootSqlApplication.class, args);
+    }
+
+}

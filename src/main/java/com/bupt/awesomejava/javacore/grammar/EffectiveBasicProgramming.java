47672a0fd5e51ef479812dd436c80f75188fa0ec
jason <591831416@qq.com>
2024-01-29 10:48:29 +0800

    update-20240129

--- src/main/java/com/bupt/awesomejava/javacore/grammar/EffectiveBasicProgramming.java
+++ src/main/java/com/bupt/awesomejava/javacore/grammar/EffectiveBasicProgramming.java
@@ -2,18 +2,25 @@ package com.bupt.awesomejava.javacore.grammar;
 
 public class EffectiveBasicProgramming {
 
+    public static final double CM_PER_INCH = 2.54;
+
+    enum Size {small, medium, large, extra_large};
+
     /**
-     *
+     * 3.1 一个简单的Java应用程序
+     * 3.2 注释
      * @param args
      */
     public static void main(String[] args) {
         // EffectiveBasicProgramming.method01();
 
-        EffectiveBasicProgramming.method02();
+        // EffectiveBasicProgramming.method02();
 
+        EffectiveBasicProgramming.method03();
     }
 
     /**
+     * 3.3 数据类型
      * 数据类型
      */
     public static void method01() {
@@ -43,7 +50,7 @@ public class EffectiveBasicProgramming {
     }
 
     /**
-     * 变量与常量
+     * 3.4 变量与常量
      */
     public static void method02() {
         System.out.println(Character.isJavaIdentifierStart('%'));
@@ -52,6 +59,26 @@ public class EffectiveBasicProgramming {
         System.out.println(number);
         var str = "just a test";
         System.out.println(str);
+        Size s = Size.small;
+    }
+
+    /**
+     * 3.5 运算符
+     */
+    public static void method03() {
+        double number = 12.23d;
+        System.out.println(number / 0);
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println("y = " + y);
+        double z = Math.pow(x, 3);
+        System.out.println("z = " + z);
+        int a = -12;
+        System.out.println(a % 2);
+        System.out.println(Math.floorMod(a, 2));
+        // StrictMath类，实现了可自由分发的数学库，www.netlib.org/fdlibm
+        // System.out.println(Math.multiplyExact(1000000000, 3));
+        System.out.println(1000000000 * 3);
 
     }
 }
